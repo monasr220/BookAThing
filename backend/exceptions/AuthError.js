88f7d9f684ccf5c1a723b2181baa9ae5f62c48bc
ANAS KILANY <anaskilany011@gmail.com>
2026-09-14 10:43:30 +0300

    remove backend - frontend only

--- backend/exceptions/AuthError.js
+++
@@ -1,9 +0,0 @@
-const AppError = require('./AppError');
-
-class AuthError extends AppError {
-    constructor(message = "Authentication Failed") {
-        super(message, 401);
-    }
-}
-
-module.exports = AuthError;
--- backend/exceptions/NotFoundError.js
+++
@@ -1,9 +0,0 @@
-const AppError = require('./AppError');
-
-class NotFoundError extends AppError {
-    constructor(resource = "Resource") {
-        super(`${resource} not found`, 404);
-    }
-}
-
-module.exports = NotFoundError;
